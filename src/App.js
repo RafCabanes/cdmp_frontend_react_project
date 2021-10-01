6d08b53d1aefa33175457a32687335322fb13592
Ubuntu <ubuntu@ip-172-31-41-240.ap-southeast-1.compute.internal>
2021-10-01 03:24:43 +0000

    added code from lecture to App.js to test DadJoke fetch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+// import "./styles.css";
+import { useState, useEffect } from "react";
 
-function App() {
+export default function App() {
+  const [dadJoke, setDadJoke] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const getData = async () => {
+    setIsLoading(true);
+    setDadJoke("");
+    const response = await fetch("https://icanhazdadjoke.com/", {
+      headers: {
+        Accept: "application/json"
+      }
+    });
+    const data = await response.json();
+    setDadJoke(data.joke);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isLoading && <h1>currently fetching dad joke...</h1>}
+      <h1>{dadJoke}</h1>
+      <button onClick={getData}>Get new dad joke</button>
     </div>
   );
 }
-
-export default App;
