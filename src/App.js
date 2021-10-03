378e8812efb9333f3cc2978f07afc42dbb38e8fe
Ubuntu <ubuntu@ip-172-31-41-240.ap-southeast-1.compute.internal>
2021-10-03 13:15:07 +0000

    updated src/App.js to fetch card from Magic the Gathering card api

--- src/App.js
+++ src/App.js
@@ -3,15 +3,14 @@ import { useState, useEffect } from "react";
 import React from "react";
 
 export default function App() {
-    const[ duck, setDuck ] = useState("default duck");
-    const [isLoading, setIsLoading] = useState(false);
-
+  const[ duck, setDuck ] = useState("default duck");
+  const[ card, setCard ] = useState("no card chosen");
+  const [isLoading, setIsLoading] = useState(false);
+  
   const getData = async () => {
     setIsLoading(true);
-    // console.log(duck);
-    // const url = "https://random-d.uk/api/random";
     try {
-      const url = "https://thronesapi.com/api/v2/Characters/11";
+      const url = "https://thronesapi.com/api/v2/Characters/13";
       // const url = "https://random-d.uk/api/random";
       const response = await fetch(url, {
         headers: {
@@ -37,24 +36,55 @@ export default function App() {
     } catch (e) {
       console.log(e.message);
     }
-    
-    
     // const response = await fetch(url);
     // console.log(response);
     // const receivedDuck = await response.json();
     // setDuck(receivedDuck.url[0]);
     setIsLoading(false);
   }
+  
+  const getCard = async () => {
+    setIsLoading(true);
+    const url = "https://api.magicthegathering.io/v1/cards?supertypes=legendary&types=creature&colors=black,white&pageSize=2";
+    try {
+      const response = await fetch(url, {
+        headers: {
+          Accept: "application/json"
+        }
+      }).then(
+        async response => {
+          // console.log(response);
+          const cardResponse = await response.json();
+          // console.log(cardResponse.cards);
+          for(var cardItem in cardResponse.cards) {
+            console.log("index: " + cardItem);
+            console.log(cardResponse.cards[cardItem]);
+          }
+          // console.log(cardResponse.cards[0].name);
+          
+        }
+        
+        )
+    } catch(e) {
+      console.log(e.message)
+    }
+    setIsLoading(false);
+  }
+  
+  function printCards(card, index) {
+    console.log(card)
+  }
   // useEffect exactly once
   useEffect(() => {
-    getData();
+    // getData();
+    getCard();
   }, []);
 
   return (
     <div className="App">
-      {isLoading && <h1>currently fetching duck</h1>}
+      {isLoading && <h1>currently fetching response</h1>}
       <h1>{duck}</h1>
-      <button onClick={getData}>Get new dad joke</button>
+      <button onClick={getCard}>Get new response</button>
     </div>
   );
 }
