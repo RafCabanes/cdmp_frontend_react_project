b3a5dc14dbee0eca0aa68a87970e96f034a91a83
Ubuntu <ubuntu@ip-172-31-41-240.ap-southeast-1.compute.internal>
2021-10-04 09:45:44 +0000

    updated src/App.js to list all search results in console

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import React from "react";
 export default function App() {
   const[ duck, setDuck ] = useState("default duck");
   const[ card, setCard ] = useState("no card chosen");
+  const[ cardName, setCardName] = useState("defaultcard")
+  
   const [isLoading, setIsLoading] = useState(false);
   
   const getData = async () => {
@@ -41,11 +43,21 @@ export default function App() {
     // const receivedDuck = await response.json();
     // setDuck(receivedDuck.url[0]);
     setIsLoading(false);
-  }
+  };
+  
+  const makeUrl = (props) => {
+    const baseUrl = "https://api.magicthegathering.io/v1/cards?";
+    const cardUrlName = "name=" + cardName;
+    // const cardUrlLanguage = "&language=" + cardLanguage;
+    const workingUrl = baseUrl + cardUrlName;
+    return workingUrl
+  };
   
   const getCard = async () => {
     setIsLoading(true);
-    const url = "https://api.magicthegathering.io/v1/cards?supertypes=legendary&types=creature&colors=black,white&pageSize=2";
+    console.log("searching for "+ cardName);
+    // const url = "https://api.magicthegathering.io/v1/cards?supertypes=legendary&types=creature&colors=black,white&pageSize=2";
+    const url = makeUrl(cardName);
     try {
       const response = await fetch(url, {
         headers: {
@@ -55,11 +67,13 @@ export default function App() {
         async response => {
           // console.log(response);
           const cardResponse = await response.json();
-          // console.log(cardResponse.cards);
+          // foreach cardItem in cardResponse.cards array
           for(var cardItem in cardResponse.cards) {
             console.log("index: " + cardItem);
             console.log(cardResponse.cards[cardItem]);
           }
+          
+          // name of first card result
           // console.log(cardResponse.cards[0].name);
           
         }
@@ -71,9 +85,6 @@ export default function App() {
     setIsLoading(false);
   }
   
-  function printCards(card, index) {
-    console.log(card)
-  }
   // useEffect exactly once
   useEffect(() => {
     // getData();
@@ -83,8 +94,18 @@ export default function App() {
   return (
     <div className="App">
       {isLoading && <h1>currently fetching response</h1>}
-      <h1>{duck}</h1>
-      <button onClick={getCard}>Get new response</button>
+      <div className="searchbox">
+        <h1>Type card name in searchbox</h1>
+        <input
+          type="text"
+          id="cardNameInputBox"
+          onChange={(event) => setCardName(event.target.value)}
+        />
+        <button onClick={getCard}>Search Card</button>
+      </div>
+      
+      <div className="cardDisplayBox">
+      </div>
     </div>
   );
 }
